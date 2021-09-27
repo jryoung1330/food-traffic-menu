8e8f476f9c37cf47719d5ae17a08bf669e44b98f
Jordan Young <jryoung1330@gmail.com>
2021-09-26 22:24:30 -0400

    fixed update menu

--- src/main/java/com/foodtraffic/menu/repository/MenuItemRepository.java
+++ src/main/java/com/foodtraffic/menu/repository/MenuItemRepository.java
@@ -10,8 +10,6 @@ import java.util.List;
 @Repository
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
 
-    MenuItem getById(Long menuItemId);
-
     boolean existsByIdAndMenuId(Long id, Long menuId);
 
     @Query(value = "select * from MENU_ITEM mi" +
